51d8637bc7997302513a8a3ce37c73c8ff0d98dc
Bry Cox <carl.cc@gmail.com>
2019-06-14 07:42:36 +0000

    Done.

--- app.rb
+++ app.rb
@@ -11,6 +11,10 @@ class App < Sinatra::Base
   end
 
   post '/puppy' do
+    Puppy.new(params[:name], params[:breed], params[:age])
+    @name = params[:name]
+    @breed = params[:breed]
+    @age = params[:age]
     erb :display_puppy
   end
 
