8043e5ecfa1798b61a358fcc54ce8cc2323a65ad
Lauren McCoy <lmccoy8911@gmail.com>
2019-08-18 20:25:51 +0000

    Done.

--- app.rb
+++ app.rb
@@ -11,7 +11,9 @@ class App < Sinatra::Base
   end 
   
   post '/puppy' do
-    Puppy.new(params)
+    @name = params[:name]
+    @age = params[:age]
+    @breed = params[:breed]
     erb :display_puppy
   end 
 end
