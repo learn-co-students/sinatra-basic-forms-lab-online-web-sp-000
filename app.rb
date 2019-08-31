94d04eba62ba8396c41a56cdb204164af9b37657
kevin lang <langkevina@gmail.com>
2019-08-31 23:09:50 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,5 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/' do
+      erb :index
+    end
+
+    post '/puppy' do
+      @name = params[:name]
+      @breed = params[:breed]
+      @age = params[:age]
+      # @analyzed_text = TextAnalyzer.new(params[:user_text])
+
+      erb :display_puppy
+    end
+    get '/new' do
+      erb :create_puppy
+    end
 
 end
