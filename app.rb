b5a319a4f7d50293465c39cc23c6c94c1e448438
Tracy Reuther <tracyreuther@gmail.com>
2019-09-11 09:14:39 +0000

    Done.

--- app.rb
+++ app.rb
@@ -11,7 +11,9 @@ class App < Sinatra::Base
   end
 
   post '/puppy' do
-
+    @name = params[:name]
+    @breed = params[:breed]
+    @months_old = params[:months_old]
 
     erb :display_puppy
   end
