600a38c5d2e4adb61b5a9e952bb2a552c5cdebbc
thisisSDK <25039980+thisisSDK@users.noreply.github.com>
2019-10-23 14:25:51 -0400

    adds directional mark up to project files

--- app.rb
+++ app.rb
@@ -1,5 +1,8 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-
+  #Get request loads homepage
+  #homepage is main route to /
+  #link to view in index.erb
+  
 end
---
+++ models/puppy.rb
@@ -0,0 +1,9 @@
+class Puppy
+
+#should have name
+#should have breed
+#should have age - months_old
+
+#don't forget attr accessors/readers
+  
+end
