af9421705464428de82f107b106c0499825d6f82
thisisSDK <25039980+thisisSDK@users.noreply.github.com>
2019-10-23 15:03:21 -0400

    links views. Dynamic links for display_puppy

--- app.rb
+++ app.rb
@@ -5,15 +5,17 @@ class App < Sinatra::Base
   #homepage is main route to /
   #link to view in index.erb
 
-    get "/" do
-      erb :index
-    end
-    get '/new' do
-      erb :create_puppy
-    end
+  get '/' do
+    erb :index
+  end
 
-    post '/puppy' do
-      @puppy = Puppy.new(params['name']), (params['breed']), (params['months_old'])
-      erb :display_puppy
-    end
+  get '/new' do
+    erb :create_puppy
+  end
+
+  post '/puppy' do
+    @puppy = Puppy.new(params['name'], params['breed'], params['months_old'])
+    erb :display_puppy
+  end
+
 end
--- models/puppy.rb
+++ models/puppy.rb
@@ -1,17 +1,16 @@
-class Puppy
-
-attr_accessor :name, :breed, :months_old
-
-def initialize(name, breed, months_old)
-  @name = name
-  @breed = breed
-  @months_old = months_old 
-end
-
 #should have name
 #should have breed
 #should have age - months_old
 
 #don't forget attr accessors/readers
 
+
+class Puppy
+attr_accessor :name, :breed, :months_old
+
+def initialize(name, breed, months_old)
+  @name = name
+  @breed = breed
+  @months_old = months_old
+end
 end
