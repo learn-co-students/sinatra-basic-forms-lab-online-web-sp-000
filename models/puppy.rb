c01e62d9bd021a9b9e82de9ce60fe30d204680dd
thisisSDK <25039980+thisisSDK@users.noreply.github.com>
2019-10-23 14:30:05 -0400

    builds Puppy class in puppy.rb

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,9 +1,17 @@
 class Puppy
 
+attr_accessor :name, :breed, :months_old
+
+def initialize(name, breed, months_old)
+  @name = name
+  @breed = breed
+  @months_old = months_old 
+end
+
 #should have name
 #should have breed
 #should have age - months_old
 
 #don't forget attr accessors/readers
-  
+
 end
