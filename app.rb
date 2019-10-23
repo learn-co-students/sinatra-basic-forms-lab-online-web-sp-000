4c0f6fa80908c433efc32244da4882113a05b76d
thisisSDK <25039980+thisisSDK@users.noreply.github.com>
2019-10-23 14:48:38 -0400

    creates get and post requests in app.rb

--- app.rb
+++ app.rb
@@ -4,5 +4,16 @@ class App < Sinatra::Base
   #Get request loads homepage
   #homepage is main route to /
   #link to view in index.erb
-  
+
+    get "/" do
+      erb :index
+    end
+    get '/new' do
+      erb :create_puppy
+    end
+
+    post '/puppy' do
+      @puppy = Puppy.new(params['name']), (params['breed']), (params['months_old'])
+      erb :display_puppy
+    end
 end
