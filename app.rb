1fae21d6ddd7ca85c88eeda80624044eecedf07e
jake0988 <jake098877@gmail.com>
2020-07-10 18:47:48 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,5 +1,35 @@
+require 'pry'
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/' do
 
+        erb :index
+    end
+
+    get '/create_puppy' do
+
+        erb :create_puppy
+    end
+
+    post '/create_puppy' do
+        @puppy = params
+        erb :display_puppy
+    end
+
+    get '/new' do
+
+        erb :new
+    end
+
+    get '/puppy' do
+    
+        erb "puppy"
+    end
+
+    post '/puppy' do
+        @puppy = params
+         binding.pry
+        erb:puppy
+    end
 end
