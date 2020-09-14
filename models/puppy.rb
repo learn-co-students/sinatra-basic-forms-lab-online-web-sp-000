2b6bd4374e74b462026213a87c3f3fb065876e96
sparsell <hello@sparsell.com>
2020-09-14 11:46:15 -0400

    Done.

--- models/puppy.rb
+++ models/puppy.rb
@@ -3,7 +3,7 @@ class Puppy
     attr_reader :breed
     attr_accessor :name, :age
 
-    def initialize(name, breed, age)
+    def initialize(params)
         @name = params[:name]
         @breed = params[:breed]
         @age = params[:age]
