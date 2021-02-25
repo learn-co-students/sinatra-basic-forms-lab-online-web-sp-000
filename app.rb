406771fe3cf943e86fae5b548cf55ea9505bb664
Jung Lee <lee.jung.s95@gmail.com>
2021-02-25 19:33:30 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,5 +1,21 @@
 require_relative 'config/environment'
+require_relative 'models/puppy'
 
 class App < Sinatra::Base
 
+    get '/' do
+        erb :index
+    end
+
+    get '/new' do
+        erb :create_puppy
+    end
+
+    post '/puppy' do
+        @puppy = Puppy.new(@name, @breed, @age)
+        @puppy.name = params[:name]
+        @puppy.breed = params[:breed]
+        @puppy.age = params[:age]
+        erb :display_puppy
+    end
 end
